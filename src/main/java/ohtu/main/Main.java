7b0b4e0a2f7c2ddcdabd823483b7fa9811f32090
Emil Andersson <anderssone0@gmail.com>
2017-11-11 16:17:48 +0200

    Checkstyle

--- src/main/java/ohtu/main/Main.java
+++ src/main/java/ohtu/main/Main.java
@@ -1,12 +1,11 @@
 package ohtu.main;
 
-import ohtu.ohtuvarasto.Varasto;
 
 public class Main {
 
     public static void main(String[] args) {
 
-
+        System.out.println("Ei tyhjä enää");
 
     }
 }
